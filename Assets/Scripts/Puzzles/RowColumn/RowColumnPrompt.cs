38ac3c804e250efcd4bb685f704f491820ae90a6
André Monteiro <andremgmonteiro01@gmail.com>
2022-12-13 20:32:44 +0000

    RowColumnSequencer Puzzle Base Implementation

---
+++ Assets/Scripts/Puzzles/RowColumn/RowColumnPrompt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UIElements;
+
+public class RowColumnPrompt : VisualElement
+{
+    public readonly Label[] prompt;
+    public int index;
+    public bool finished;
+
+    public RowColumnPrompt() : this(6) { }
+
+    public RowColumnPrompt(int depth)
+    {
+        prompt = new Label[depth];
+        AddToClassList("row");
+        for (int i = 0; i < depth; i++)
+        {
+            prompt[i] = new Label();
+            prompt[i].AddToClassList("prompt-label");
+            Add(prompt[i]);
+        }
+    }
+
+    public new class UxmlFactory : UxmlFactory<RowColumnPrompt, UxmlTraits> { }
+}
