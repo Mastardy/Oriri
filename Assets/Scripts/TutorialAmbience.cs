25a267e56d3f1c13e17450426348298e7fd17e34
André <andremgmonteiro01@gmail.com>
2023-01-30 18:03:51 +0000

    Added Tutorial Videos and Main Menu Background Image

--- Assets/Scripts/TutorialAmbience.cs
+++ Assets/Scripts/TutorialAmbience.cs
@@ -5,8 +5,22 @@ using UnityEngine.Rendering.Universal;
 public class TutorialAmbience : MonoBehaviour
 {
     private ChromaticAberration aberration;
+    private LensDistortion distortion;
     
     private void Awake() => GetComponent<Volume>().profile.TryGet(out aberration);
+    private void Start() => GetComponent<Volume>().profile.TryGet(out distortion);
 
     private void Update() => aberration.intensity.value = 0.25f + (Mathf.PerlinNoise(Time.time * 6, Time.time * 6) / 4.0f) * 3.0f;
+
+    public void RemoveLensDistortion()
+    {
+        distortion.active = false;
+        aberration.active = false;
+    }
+
+    public void AddLensDistortion()
+    {
+        distortion.active = true;
+        aberration.active = true;
+    } 
 }
